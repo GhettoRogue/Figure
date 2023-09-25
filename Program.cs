1d018db3def64f6cfb428cfd916b5a08bf44adbe
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-09-25 09:23:58 +0400

    upd

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
 ﻿using Figure.app;
 
-var figure = new Circle(20.5);
-Console.WriteLine(figure.CalcArea());
+var figure1 = new Circle(20.5);
+Console.WriteLine(figure1.CalcArea());
+
+var figure2 = new Triangle(5, 6,7);
+Console.WriteLine(figure2.CalcArea());
+
+var figure3 = new Rectangle(10.5, 10.5);
+Console.WriteLine(figure3.CalcArea());
---
+++ Triangle.cs
@@ -0,0 +1,23 @@
+﻿namespace Figure.app;
+
+public class Triangle : Figure
+{
+    private readonly double a;
+    private readonly double b;
+    private readonly double c;
+
+    public Triangle(double a, double b, double c)
+    {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public override double CalcArea()
+    {
+        var perimeter = a + b + c;
+        var halfPerimeter = perimeter / 2;
+        var square = Math.Sqrt(halfPerimeter * (halfPerimeter - a) * (halfPerimeter - b) * (halfPerimeter - c));
+        return square;
+    }
+}
