bbba0cd6279cd5e76714a13d0fe7a633f5545d7a
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-09-25 20:55:24 +0400

    upd

--- Triangle.cs
+++ Triangle.cs
@@ -2,15 +2,23 @@
 
 public class Triangle : Figure
 {
-    public double A { get; init; }
-    public double B { get; init; }
-    public double C { get; init; }
+    private double A { get; init; }
+    private double B { get; init; }
+    private double C { get; init; }
+
+    public Triangle(double a, double b, double c)
+    {
+        A = a;
+        B = b;
+        C = c;
+    }
 
     public override double CalcArea()
     {
         var perimeter = A + B + C;
         var halfPerimeter = perimeter / 2;
         var square = Math.Sqrt(halfPerimeter * (halfPerimeter - A) * (halfPerimeter - B) * (halfPerimeter - C));
+        
         return square;
     }
 }
