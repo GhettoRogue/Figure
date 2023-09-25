77fd76282e9e492c6525c20f3469f4ab2c672a06
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-09-25 20:55:14 +0400

    upd

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
 ﻿using Figure.app;
 
-var figure1 = new Circle { Radius = 20.5 };
-Console.WriteLine(figure1.CalcArea());
+/*var circle = new Circle { Radius = 20.5 };
+Console.WriteLine(circle.CalcArea());
 
-var figure2 = new Triangle { A = 5, B = 6, C = 7 };
-Console.WriteLine(figure2.CalcArea());
+var triangle = new Triangle { A = 5, B = 6, C = 7 };
+Console.WriteLine(triangle.CalcArea());
 
-var figure3 = new Rectangle { Length = 10.5, Width = 10.5 };
-Console.WriteLine(figure3.CalcArea());
+var rectangle = new Rectangle { Length = 10.5, Width = 10.5 };
+Console.WriteLine(rectangle.CalcArea());*/
+
+var circle = new Circle(10);
+Console.WriteLine(Geometry.GetArea(circle));
+
+var triangle = new Triangle (5,6,7);
+Console.WriteLine(Geometry.GetArea(triangle));
+
+var rectangle = new Rectangle(10, 10);
+Console.WriteLine(Geometry.GetArea(rectangle));
+
