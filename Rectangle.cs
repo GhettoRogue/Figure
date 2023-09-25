aee296eae9d4f47b76e787543c562f2c50ae990a
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-09-25 09:23:48 +0400

    add Rectangle.cs

---
+++ Rectangle.cs
@@ -0,0 +1,18 @@
+﻿namespace Figure.app;
+
+public class Rectangle : Figure
+{
+    private readonly double length;
+    private readonly double width;
+
+    public Rectangle(double length, double width)
+    {
+        this.length = length;
+        this.width = width;
+    }
+
+    public override double CalcArea()
+    {
+        return length * width;
+    }
+}
