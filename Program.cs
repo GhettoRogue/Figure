c60a7d4934626ca235490d55fdd4ffd8cddde846
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-09-25 09:53:52 +0400

    upd context

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿using Figure.app;
 
-var figure1 = new Circle(20.5);
+var figure1 = new Circle { Radius = 20.5 };
 Console.WriteLine(figure1.CalcArea());
 
-var figure2 = new Triangle(5, 6,7);
+var figure2 = new Triangle { A = 5, B = 6, C = 7 };
 Console.WriteLine(figure2.CalcArea());
 
-var figure3 = new Rectangle(10.5, 10.5);
+var figure3 = new Rectangle { Length = 10.5, Width = 10.5 };
 Console.WriteLine(figure3.CalcArea());
