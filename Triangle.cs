725e24fe999b9e5ebf954aab0865745cd22bf1c1
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-09-25 09:53:44 +0400

    change Triangle.cs with properties

--- Triangle.cs
+++ Triangle.cs
@@ -2,22 +2,15 @@
 
 public class Triangle : Figure
 {
-    private readonly double a;
-    private readonly double b;
-    private readonly double c;
-
-    public Triangle(double a, double b, double c)
-    {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-    }
+    public double A { get; init; }
+    public double B { get; init; }
+    public double C { get; init; }
 
     public override double CalcArea()
     {
-        var perimeter = a + b + c;
+        var perimeter = A + B + C;
         var halfPerimeter = perimeter / 2;
-        var square = Math.Sqrt(halfPerimeter * (halfPerimeter - a) * (halfPerimeter - b) * (halfPerimeter - c));
+        var square = Math.Sqrt(halfPerimeter * (halfPerimeter - A) * (halfPerimeter - B) * (halfPerimeter - C));
         return square;
     }
 }
